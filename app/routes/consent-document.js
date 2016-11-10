c98017a71e8443c989a6e2c7a408ca777b13ea95
Chris Ortman <chris-ortman@uiowa.edu>
2016-11-09 18:44:29 -0600

    Semantic UI and model

--- app/routes/consent-document.js
+++ app/routes/consent-document.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
+let consentDocument = {
+  version: 'V2',
+  lastPublished: '02/01/2016',
+  sections: [
+    title: 'Overview'
+  ]
+};
+
 export default Ember.Route.extend({
+  model() {
+    return consentDocument;
+  }
 });
