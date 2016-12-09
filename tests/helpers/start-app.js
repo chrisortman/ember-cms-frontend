87e2bd82be1a731c28a24676282db5598877adfa
Chris Ortman <chris-ortman@uiowa.edu>
2016-12-09 13:28:20 -0600

    Ember cli 2.10

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,16 +1,14 @@
 import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
-import PouchDB from 'pouchdb';
 
 export default function startApp(attrs) {
   let application;
 
-  let attributes = Ember.merge({}, config.APP);
-  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+  // use defaults, but you can override
+  let attributes = Ember.assign({}, config.APP, attrs);
 
   let db = new PouchDB('poop',{adapter: 'memory'});
-
   Ember.run(() => {
     application = Application.create(attributes);
     application.setupForTesting();
